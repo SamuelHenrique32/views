a9888c1318658fcb727f08a8b81a08e0de665604
SamuelHenrique32 <shdalmas@ucs.br>
2019-02-02 14:00:08 -0200

    Aula 31 - Passando parametros a views

--- resources/views/minhaview.blade.php
+++ resources/views/minhaview.blade.php
@@ -1,1 +1,8 @@
-<h1>Teste</h1>
+<html>
+    <head>
+        <title>Minha página</title>
+    </head>
+    <body>
+        <h1>Hello, {{$nome}} {{$sobrenome}}</h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,4 +7,27 @@ Route::get('/', function () {
 
 Route::get('/primeiraview', function () {
     return view('minhaview');
+});
+
+//parametros para views
+Route::get('/ola', function (){
+
+    return view('minhaview')
+        ->with('nome', 'Joao')
+        ->with('sobrenome', 'Silva');
+});
+
+//parametros por url
+Route::get('/ola/{nome}/{sobrenome}', function ($nome, $sobrenome){
+    /*
+    return view('minhaview')
+        ->with('nome', $nome)
+        ->with('sobrenome', $sobrenome);*/
+
+    //outra opcao para passagem de parametros: array associativo
+    /*$parametros = ['nome'=>$nome, 'sobrenome'=>$sobrenome];
+    return view('minhaview', $parametros);*/
+
+    //terceira opcao, monta array associativo sozinho atraves de compact
+    return view('minhaview', compact('nome', 'sobrenome'));
 });
