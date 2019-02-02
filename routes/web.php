72f4badd39f6641fdad65a05af03d3de27c8614d
SamuelHenrique32 <shdalmas@ucs.br>
2019-02-02 14:20:52 -0200

    Aula 32 - Verificando se uma view existe

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,13 @@ Route::get('/ola/{nome}/{sobrenome}', function ($nome, $sobrenome){
     //terceira opcao, monta array associativo sozinho atraves de compact
     return view('minhaview', compact('nome', 'sobrenome'));
 });
+
+//verificar se view existe
+Route::get('/email/{email}', function ($email){
+
+    if(View::exists('email')){
+        return view('email', compact('email'));
+    } else{
+        return view('erro');
+    }
+});
