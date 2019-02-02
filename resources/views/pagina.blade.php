8cbf9af5214bc6fae6c4dfe09cdda46c530aa0bb
SamuelHenrique32 <shdalmas@ucs.br>
2019-02-02 19:22:20 -0200

    Aula 37 - Customizando chamadas a componentes

--- resources/views/pagina.blade.php
+++ resources/views/pagina.blade.php
@@ -6,13 +6,33 @@
 
     <body>
 
-        @component('components.meucomponente', ['tipo'=>'danger', 'titulo'=>'Erro fatal'])
+        @alerta(['tipo'=>'danger', 'titulo'=>'Erro fatal'])
             <strong>Erro: </strong> Sua mensagem de erro.
-        @endcomponent
+        @endalerta
 
-        @component('components.meucomponente', ['tipo'=>'danger', 'titulo'=>'Erro fatal'])
+        @alerta(['tipo'=>'warning', 'titulo'=>'Erro fatal'])
             <strong>Erro: </strong> Sua mensagem de erro.
-        @endcomponent
+        @endalerta
+
+        @alerta(['tipo'=>'success', 'titulo'=>'Erro fatal'])
+            <strong>Erro: </strong> Sua mensagem de erro.
+        @endalerta
+
+        @alerta(['tipo'=>'primary', 'titulo'=>'Erro fatal'])
+            <strong>Erro: </strong> Sua mensagem de erro.
+        @endalerta
+
+        @alerta(['tipo'=>'secondary', 'titulo'=>'Erro fatal'])
+            <strong>Erro: </strong> Sua mensagem de erro.
+        @endalerta
+
+        @alerta(['tipo'=>'info', 'titulo'=>'Erro fatal'])
+            <strong>Erro: </strong> Sua mensagem de erro.
+        @endalerta
+
+        @alerta(['tipo'=>'dark', 'titulo'=>'Erro fatal'])
+            <strong>Erro: </strong> Sua mensagem de erro.
+        @endalerta
 
         <script src="{{ URL::to('js/app.js') }}" type="text/javascript"></script>
 
