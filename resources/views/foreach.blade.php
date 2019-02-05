c98e3ca1a2870850b320f8af59658017f51b130f
SamuelHenrique32 <shdalmas@ucs.br>
2019-02-04 23:27:39 -0200

    Aula 42 - Atributos dos lacos

--- resources/views/foreach.blade.php
+++ resources/views/foreach.blade.php
@@ -1,5 +1,32 @@
-<h1>Loop Foreach - Arrays Associativos</h1>
+@extends('layout.meulayout')
 
-@foreach($produtos as $p)
-    <p>{{ $p['id'] }}: {{ $p['nome'] }}</p>
-@endforeach
+@section('minha_secao_produtos')
+
+    <h1>Loop Foreach - Arrays Associativos</h1>
+
+    @foreach($produtos as $p)
+        <p>{{ $p['id'] }}: {{ $p['nome'] }}</p>
+    @endforeach
+
+    <hr>
+
+    @foreach($produtos as $p)
+        <p>
+            {{ $p['id'] }}: {{ $p['nome'] }}
+            @if($loop->first)
+                (primeiro)
+            @endif
+            @if($loop->last)
+                (ultimo)
+            @endif
+            <span class="badge badge-secondary">
+                {{$loop->index}} / {{$loop->count}} / {{$loop->remaining}}
+            </span>
+
+            <span class="badge badge-warning">
+                {{$loop->iteration}} / {{$loop->count}}
+            </span>
+        </p>
+    @endforeach
+
+@endsection
